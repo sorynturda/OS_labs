913901f41127929702c6fbd259c2bfc475090c57
sorynturda <s.turda12@gmail.com>
2024-04-16 11:51:42 +0300

    semafor adaugat la pb 1:

--- 08/probleme_propuse/1.c
+++ 08/probleme_propuse/1.c
@@ -15,30 +15,40 @@
 
 long count;
 
-void *thread_function(void *unused)
+void *thread_function(void *p)
 {
+    sem_t *sem = (sem_t *)p;
     int i;
     long aux;
     for (i = 0; i < M; i++) {
+        sem_wait(sem);
         aux = count;
         aux++;
         usleep(random() % 10);
         count = aux;
+        sem_post(sem);
     }
     return NULL;
 }
 
 int main(void)
 {
+    sem_t sem;
     pthread_t tids[NR_THREADS];
     int i;
-
     srand(time(NULL));
 
-    for(int i=0;i<NR_THREADS;i++)
-        pthread_create(&tids[i], NULL, thread_function, NULL);
-    for(int i=0;i<NR_THREADS;i++)
+    if (sem_init(&sem, 0, 1) != 0) {
+        perror("could not init the semaphore");
+        return -1;
+    }
+
+    for (int i = 0; i < NR_THREADS; i++)
+        pthread_create(&tids[i], NULL, thread_function, &sem);
+
+    for (int i = 0; i < NR_THREADS; i++)
         pthread_join(tids[i], NULL);
+    sem_destroy(&sem);
     printf("%ld\n", count);
     return 0;
 }
